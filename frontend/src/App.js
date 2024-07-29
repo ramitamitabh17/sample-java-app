93002b890950818d9a95f8456d091ad0522bc814
ibansal0303 <ishank.bansal@tothenew.com>
2024-07-29 08:55:18 +0530

    app updated

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('/api/message')
+      .then(response => response.json())
+      .then(data => setMessage(data.message));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to the 3-tier Application</h1>
+        <p>{message}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
